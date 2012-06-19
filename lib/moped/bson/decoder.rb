54f65c0e9013befcf949fb0d11a79d614872805f
Bernerd Schaefer <bj.schaefer@gmail.com>
2012-06-19 13:44:14 +0200

    Decoder uses StringIO

--- lib/moped/bson/decoder.rb
+++ lib/moped/bson/decoder.rb
@@ -7,33 +7,18 @@ module Moped
 
       include Moped::BSON::Assertions
 
-      attr_accessor :buffer, :pos, :buffer_length
+      attr_accessor :buffer
 
       def initialize(buffer)
-        @buffer = buffer
-        @buffer_length = @buffer.length
-        @pos = 0
+        @buffer = StringIO.new buffer
       end
 
       def read_cstring
-        assert index = @buffer.index("\x00", @pos),
-          "Expected cstring to end with a null byte"
-
-        string = utf8_decode read_bytes(index - @pos)
-
-        assert read_byte == 0,
-          "Expected cstring to end with a null byte"
-
-        string
+        utf8_decode @buffer.gets("\x00").chop!
       end
 
       def read_string
-        string = utf8_decode read_bytes(read_int32 - 1)
-
-        assert read_byte == 0,
-          "Expected string to end with a null byte"
-
-        string
+        utf8_decode read_bytes(read_int32).chop!
       end
 
       def read_int32
@@ -49,21 +34,11 @@ module Moped
       end
 
       def read_bytes(bytes)
-        assert @pos + bytes < buffer_length,
-          "Expected #{bytes} bytes to be available"
-
-        current, @pos = @pos, @pos + bytes
-
-        @buffer[current, bytes]
+        buffer.read bytes
       end
 
       def read_byte
-        assert @pos < buffer_length,
-          "Expected 1 byte to be available"
-
-        current, @pos = @pos, @pos + 1
-
-        @buffer.getbyte current
+        buffer.getbyte
       end
 
       private
